15cc3489c191bb4a6824edbec8b90b53422de7fa
Mingu Kim <mingufk@gmail.com>
2022-12-12 23:08:24 +0900

    Add keyboard events

--- virtual-keyboard/src/js/keyboard.js
+++ virtual-keyboard/src/js/keyboard.js
@@ -1,25 +1,63 @@
 export class Keyboard {
   #switchEl;
   #fontSelectEl;
+  #containerEl;
+  #keyboardEl;
+  #inputGroupEl;
+  #inputEl;
+
   constructor() {
     this.#assignElement();
     this.#addEvent();
   }
 
   #assignElement() {
-    this.#switchEl = document.querySelector("#switch");
-    this.#fontSelectEl = document.querySelector("#font");
+    this.#containerEl = document.querySelector("#container");
+    this.#switchEl = this.#containerEl.querySelector("#switch");
+    this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
+    this.#inputEl = this.#containerEl.querySelector("#input");
   }
 
   #addEvent() {
-    this.#switchEl.addEventListener("change", (e) => {
-      document.documentElement.setAttribute(
-        "theme",
-        e.target.checked ? "dark-mode" : ""
-      );
-    });
-    this.#fontSelectEl.addEventListener("change", (e) => {
-      document.body.style.fontFamily = e.target.value;
-    });
+    this.#switchEl.addEventListener("change", this.#changeTheme);
+    this.#fontSelectEl.addEventListener("change", this.#changeFont);
+    document.addEventListener("keydown", this.#handleKeyDown.bind(this));
+    document.addEventListener("keyup", this.#handleKeyUp.bind(this));
+    this.#inputEl.addEventListener("input", this.#handleInput);
+  }
+
+  #changeTheme(e) {
+    document.documentElement.setAttribute(
+      "theme",
+      e.target.checked ? "dark-mode" : ""
+    );
+  }
+
+  #changeFont(e) {
+    document.body.style.fontFamily = e.target.value;
+  }
+
+  #handleKeyDown(e) {
+    this.#inputGroupEl.classList.toggle(
+      "error-message",
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(e.key)
+    );
+    if (this.#keyboardEl.querySelector(`[data-code=${e.code}]`)) {
+      this.#keyboardEl
+        .querySelector(`[data-code=${e.code}]`)
+        .classList.add("active");
+    }
+  }
+
+  #handleKeyUp(e) {
+    this.#keyboardEl
+      .querySelector(`[data-code=${e.code}]`)
+      ?.classList.remove("active");
+  }
+
+  #handleInput(e) {
+    e.target.value = e.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
   }
 }
