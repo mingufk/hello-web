42231bf0f363a99812378dfea0c2b348123d037f
Mingu Kim <mingufk@gmail.com>
2022-12-16 00:42:47 +0900

    Add date lookup

--- date-picker/src/index.js
+++ date-picker/src/index.js
@@ -1,1 +1,173 @@
-console.log('Date Picker');
+class DatePicker {
+  monthData = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  #calendarDate = {
+    data: '',
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  selectedDate = {
+    data: '',
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  datePickerEl;
+  dateInputEl;
+  calendarEl;
+  calendarMonthEl;
+  prevBtnEl;
+  monthContentEl;
+  nextBtnEl;
+  calendarDatesEl;
+
+  constructor() {
+    this.initCalendarDate();
+    this.assignElement();
+    this.addEvent();
+  }
+
+  initCalendarDate() {
+    const data = new Date();
+    const date = data.getDate();
+    const month = data.getMonth();
+    const year = data.getFullYear();
+    this.#calendarDate = {
+      data,
+      date,
+      month,
+      year,
+    };
+  }
+
+  assignElement() {
+    this.datePickerEl = document.querySelector('#date-picker');
+    this.dateInputEl = this.datePickerEl.querySelector('#date-input');
+    this.calendarEl = this.datePickerEl.querySelector('#calendar');
+    this.calendarMonthEl = this.calendarEl.querySelector('#month');
+    this.prevBtnEl = this.calendarMonthEl.querySelector('#prev');
+    this.monthContentEl = this.calendarMonthEl.querySelector('#content');
+    this.nextBtnEl = this.calendarMonthEl.querySelector('#next');
+    this.calendarDatesEl = this.calendarEl.querySelector('#dates');
+  }
+
+  addEvent() {
+    this.dateInputEl.addEventListener('click', this.toggleCalendar.bind(this));
+    this.prevBtnEl.addEventListener('click', this.prevMonth.bind(this));
+    this.nextBtnEl.addEventListener('click', this.nextMonth.bind(this));
+  }
+
+  prevMonth() {
+    this.#calendarDate.month--;
+    if (this.#calendarDate.month < 0) {
+      this.#calendarDate.month = 11;
+      this.#calendarDate.year--;
+    }
+    this.updateMonth();
+    this.updateDates();
+  }
+
+  nextMonth() {
+    this.#calendarDate.month++;
+    if (this.#calendarDate.month > 11) {
+      this.#calendarDate.month = 0;
+      this.#calendarDate.year++;
+    }
+    this.updateMonth();
+    this.updateDates();
+  }
+
+  toggleCalendar() {
+    this.calendarEl.classList.toggle('active');
+    this.updateMonth();
+    this.updateDates();
+  }
+
+  updateMonth() {
+    this.monthContentEl.textContent = `${
+      this.monthData[this.#calendarDate.month]
+    } ${this.#calendarDate.year}`;
+  }
+
+  updateDates() {
+    this.calendarDatesEl.innerHTML = '';
+    const numberOfDates = new Date(
+      this.#calendarDate.year,
+      this.#calendarDate.month + 1,
+      0,
+    ).getDate();
+    const fragment = new DocumentFragment();
+    for (let i = 0; i < numberOfDates; i++) {
+      const dateEl = document.createElement('div');
+      dateEl.classList.add('date');
+      dateEl.textContent = i + 1;
+      dateEl.dataset.date = i + 1;
+      fragment.appendChild(dateEl);
+    }
+    fragment.firstChild.style.gridColumnStart =
+      new Date(this.#calendarDate.year, this.#calendarDate.month, 1).getDay() +
+      1;
+    this.calendarDatesEl.appendChild(fragment);
+    this.colorSat();
+    this.colorSun();
+    this.markToday();
+  }
+
+  markToday() {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
+    const today = currentDate.getDate();
+    if (
+      currentYear === this.#calendarDate.year &&
+      currentMonth === this.#calendarDate.month
+    ) {
+      const todayEl = this.calendarDatesEl.querySelector(
+        `[data-date="${today}"]`,
+      );
+      todayEl.classList.add('today');
+    }
+  }
+
+  colorSat() {
+    const satEls = this.calendarDatesEl.querySelectorAll(
+      `.date:nth-child(7n+${
+        7 -
+        new Date(this.#calendarDate.year, this.#calendarDate.month, 1).getDay()
+      })`,
+    );
+    for (let i = 0; i < satEls.length; i++) {
+      satEls[i].style.color = '#0f4c81';
+    }
+  }
+
+  colorSun() {
+    const sunEls = this.calendarDatesEl.querySelectorAll(
+      `.date:nth-child(7n+${
+        8 -
+        new Date(this.#calendarDate.year, this.#calendarDate.month, 1).getDay()
+      })`,
+    );
+    for (let i = 0; i < sunEls.length; i++) {
+      sunEls[i].style.color = '#bf1932';
+    }
+  }
+}
+
+new DatePicker();
