1bb8315cd85622f574aed8001977396d462239fc
Mingu Kim <mingufk@gmail.com>
2022-12-22 00:33:50 +0900

    ✨ Making an Interactive Component

--- tic-tac-toe/src/Square.js
+++ tic-tac-toe/src/Square.js
@@ -1,5 +1,13 @@
+import { useState } from 'react'
+
 function Square(props) {
-  return <button className="square">{props.value}</button>
+  const [state, setState] = useState({ value: null })
+
+  return (
+    <button className="square" onClick={() => setState({ value: 'X' })}>
+      {state.value}
+    </button>
+  )
 }
 
 export default Square
