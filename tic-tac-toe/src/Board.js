fe06484fba85708a1ded62b12c309b3b84b47201
Mingu Kim <mingufk@gmail.com>
2022-12-22 01:15:56 +0900

    ✨ Taking Turns

--- tic-tac-toe/src/Board.js
+++ tic-tac-toe/src/Board.js
@@ -2,19 +2,22 @@ import { useState } from 'react'
 import Square from './Square'
 
 function Board(props) {
-  const [state, setState] = useState({ squares: Array(9).fill(null) })
+  const [state, setState] = useState({
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  })
 
   const handleClick = (i) => {
     const squares = state.squares.slice()
-    squares[i] = 'X'
-    setState({ squares: squares })
+    squares[i] = state.xIsNext ? 'X' : 'O'
+    setState({ squares: squares, xIsNext: !state.xIsNext })
   }
 
   const renderSquare = (i) => {
     return <Square value={state.squares[i]} onClick={() => handleClick(i)} />
   }
 
-  const status = 'Next player: X'
+  const status = 'Next player: ' + (state.xIsNext ? 'X' : 'O')
 
   return (
     <div>
