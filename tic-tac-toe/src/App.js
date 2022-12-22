c683f68308328a5990324566a1acf931acd3a30c
Mingu Kim <mingufk@gmail.com>
2022-12-22 15:48:03 +0900

    ✨ Implementing Time Travel

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -3,50 +3,57 @@ import './App.css'
 import Board from './Board'
 import calculateWinner from './calculateWinner'
 
-function App(props) {
-  const [state, setState] = useState({
-    history: [{ squares: Array(9).fill(null) }],
-    xIsNext: true,
-  })
+function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }])
+  const [stepNumber, setStepNumber] = useState(0)
+  const [xIsNext, setXIsNext] = useState(true)
 
   const handleClick = (i) => {
-    const history = state.history
-    const current = history[history.length - 1]
-    const squares = current.squares.slice()
-    if (calculateWinner(squares) || squares[i]) {
+    const newHistory = history.slice(0, stepNumber + 1)
+    const newCurrent = newHistory[newHistory.length - 1]
+    const newSquares = newCurrent.squares.slice()
+    if (calculateWinner(newSquares) || newSquares[i]) {
       return
     }
-    squares[i] = state.xIsNext ? 'X' : 'O'
-    setState({
-      history: history.concat([
-        {
-          squares,
-        },
-      ]),
-      xIsNext: !state.xIsNext,
-    })
+    newSquares[i] = xIsNext ? 'X' : 'O'
+    setHistory([...newHistory, { squares: newSquares }])
+    setStepNumber(newHistory.length)
+    setXIsNext((prev) => !prev)
   }
 
-  const history = state.history
-  const current = history[history.length - 1]
+  const jumpTo = (step) => {
+    setStepNumber(step)
+    setXIsNext(step % 2 === 0)
+  }
+
+  const current = history[stepNumber]
   const winner = calculateWinner(current.squares)
+
+  const moves = history.map((step, move) => {
+    const desc = move ? 'Go to move #' + move : 'Go to game start'
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    )
+  })
 
   let status
   if (winner) {
     status = 'Winner: ' + winner
   } else {
-    status = 'Next player : ' + (state.xIsNext ? 'X' : 'O')
+    status = 'Next player : ' + (xIsNext ? 'X' : 'O')
   }
 
   return (
     <div className="App">
+      <h1>Tic Tac Toe</h1>
       <div className="App-board">
-        <h1>Tic Tac Toe</h1>
         <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="App-info">
         <div>{status}</div>
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
